881e4e4a265016374f6ed3dbd9545ca040eda754
sakshi <panchalsakshi01@gmail.com>
2023-08-11 18:15:49 +0530

    header, message section added

---
+++ client/components/customMessageForm/StandardMessageForm.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+const StandardMessageForm = () => {
+    const [message, setMessage] = useState("");
+    const [attachment, setAttachment] = useState("");
+    const [preview, setPreview] = useState("");
+  return (
+    <div className='message-form-container'>
+      {preview && (
+        <div className='message-form-preview'>
+            <img 
+             alt='message-form-preview' 
+             className='message-form-preview-image'
+             src={preview}
+             onLoad={() => URL.revokeObjectURL(preview)} 
+            />
+            <XMarkIcon
+              className="message-form-icon-x"
+              onClick={() => {
+                setPreview("");
+                setAttachment("");
+              }}
+            />
+        </div>
+      )}
+    </div>
+  )
+};
+
+export default StandardMessageForm;
