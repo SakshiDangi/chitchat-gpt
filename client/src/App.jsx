8d8263410f81e4aad73f6617827b514a25577ed5
sakshi <panchalsakshi01@gmail.com>
2023-08-11 17:12:20 +0530

    API setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Chat from "@/components/chat";
+import Chat from "../components/chat";
 function App() {
   return (                   
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/chat" element={<Chat />} />
+          <Route path="/" element={<Chat />} />
         </Routes>
       </BrowserRouter>
     </>
